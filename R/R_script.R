51c103e733b9ee5f252736c3d8fd85e5789fc440
Akshita Harale <aharale222@email.com>
2026-07-23 15:12:35 +0530

    "add more examples"

--- R/R_script.R
+++ R/R_script.R
@@ -2,11 +2,13 @@ library(usethis)
 use_git()
 use_github()
 getwd()
+
 #Example of matrix
 mat <- matrix(1:100,nrow=10)
 mat
-mat <- matrix(1:100,nrow=5)
+mat <- matrix(1:10,nrow=5)
 mat
+
 #Example of vector
 vec <- 1:100
 vec
@@ -17,8 +19,38 @@ letters
 letters <- c("A","B","C","D","E")
 letters[c(1,3)]
 letters[-c(1,3)]
+
+#Example of Char Vector
+letters <- c("A","B","C","D","E")
+letters[-c(1,3,5)]
+ToExclude <- c("A","B","E")
+ToExclude %in% letters
+meta <- c("A","B","C","D","E")
+letters[-c(1,3,5)]
+rnaseq <- c("A","c","F")
+View(mat)
+mat
+mat[5,5]
+mat [5,c(1:10)]
+View(mat)
+
+#Naming columns
+Sample <- c("Control","Experiment")
+colnames(mat) <- Sample
+mat
+mat[5,]
+mat[,"Control"]
+
+#num
+num <- 1:100
+typeof(num)
+#if you want after 4 values
+seqofnum <-seq(from=1, by=4, to=100)
+seqofnum
+seqofnum <-seq(from=1, to=10, length.out=100)
+seqofnum
+mat[3,1] ="true"
+mat
+
 library(usethis)
 use_git()
-use_github()
-usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
-use_github()
