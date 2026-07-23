8b2d08239f7b1e94bd8f83229d7be0f0597e5178
Akshita Harale <aharale222@email.com>
2026-07-23 15:07:47 +0530

    Initial commit

--- R/R_script.R
+++ R/R_script.R
@@ -2,3 +2,23 @@ library(usethis)
 use_git()
 use_github()
 getwd()
+#Example of matrix
+mat <- matrix(1:100,nrow=10)
+mat
+mat <- matrix(1:100,nrow=5)
+mat
+#Example of vector
+vec <- 1:100
+vec
+vec <- 10:100
+vec[10]
+letters <- c("A","B","C","D","E")
+letters
+letters <- c("A","B","C","D","E")
+letters[c(1,3)]
+letters[-c(1,3)]
+library(usethis)
+use_git()
+use_github()
+usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
+use_github()
